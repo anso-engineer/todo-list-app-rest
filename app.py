cbf0858247b19777aad3e83d5398c90cfc0e0a30
Andrii Sotnikov <andrew.sotnikov@zoho.com>
2025-01-12 23:59:00 +0200

    Added ip range modifications.

--- app.py
+++ app.py
@@ -16,8 +16,16 @@ def create_app():
 
     migrate = Migrate(app, db)
 
-    # CORS(app, supports_credentials=True, resources={r"/*": {"origins": r"http://localhost:517\d+"}})
-    CORS(app, supports_credentials=True, resources={r"/*": {"origins": r"http://localhost:3000"}})
+    allowed_origins = [
+        "http://10.8.0.2:3000",
+        "http://10.8.0.3:3000",
+        "http://10.8.0.4:3000",
+        "http://10.8.0.5:3000"
+
+    ]
+
+    # Enable CORS with hardcoded allowed origins
+    CORS(app, supports_credentials=True, resources={r"/*": {"origins": allowed_origins}})
 
     return app
 
